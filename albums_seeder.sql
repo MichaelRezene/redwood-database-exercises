74ae7111315505ab4f1e142afe7958785b5f77f9
michael <mgebregziabher86@gmail.com>
2017-10-10 15:04:51 -0500

    complete the Insert exercise

---
+++ albums_seeder.sql
@@ -0,0 +1,15 @@
+USE codeup_test_db;
+
+INSERT INTO albums(artist, name, release_date, sale, genre)
+VALUES
+('Whitney Houston','The Bodyguard',1992,27.4,'R&B'),
+('Santana', 'Supernatural', 1999, 20.5, 'Rock'),
+('Eagles', 'Their Greatest', 1976, 32.2, 'Soft rock'),
+('Meat Loaf', 'Bat Out of Hell', 1977, 27.9, 'Soft rock'),
+('Fleetwood Mac', 'Rumours', 1977, 21.5, 'Hard rock'),
+('Bee Gees', 'Saturday night fever', 1977, 20.6, 'Disco'),
+('Madonna', 'The immaculate Collection', 1990, 19.4, 'Pop Dance'),
+('Michael Jackson', 'Bad', 1987, 23.6, 'Pop Frunk'),
+('James Horner', 'Titanic', 1997, 18.1, 'Soundtrack'),
+('Metallica', 'Metallica', 1991, 21.2, 'Thrash metal')
+('Shania', 'Come On', 1997, 29.6, 'Country Pop');
